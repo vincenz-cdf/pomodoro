00e93a2b3fe3a7477fc07420756beffb63c78b6b
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-08-05 03:34:27 -0700

    final update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,71 @@
+//Boutons
 const activateEl = document.getElementById("activate");
+const pauseEl = document.getElementById("pause");
+
+//Minuteur
 const timerEl = document.getElementById("timer");
+
+//Fichier Mp3
 const alarmSound = document.getElementById("alarm");
 
+//Compteur
+const countEl = document.getElementById("count");
+var counter = 0;
+countEl.innerHTML = "Session : " + counter;
+
+function incrementCounter() {
+  counter++
+  countEl.innerHTML = "Session : " + counter;
+}
+
 activateEl.addEventListener("click", function(){
+  var timerWork = new Date(new Date().getTime() + 25*60000).getTime();
   activateEl.style.visibility='hidden';
-  var timeleft = 3;
+  pauseEl.style.visibility='hidden';
 
-  var downloadTimer = setInterval(function function1(){
-    timerEl.innerHTML = timeleft + 
-  " "+"seconds remaining";
+  var x = setInterval(function() {
 
-  timeleft -= 1;
-  if(timeleft <= 0){
-      clearInterval(downloadTimer);
+    var now = new Date().getTime();
+
+    var distance = timerWork - now;
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    timerEl.innerHTML = minutes + "m " + seconds + "s";
+
+    if (distance < 0) {
+      clearInterval(x);
       alarmSound.play();
-      timerEl.innerHTML = "Time is up!"
-  }
+      timerEl.innerHTML = "Time's up!";
+      activateEl.style.visibility='visible';
+      pauseEl.style.visibility='visible';
+      incrementCounter();
+    }
   }, 1000);
 });
 
+pauseEl.addEventListener("click", function() {
+  var timerBreak = new Date(new Date().getTime() + 5*60000).getTime();
+  activateEl.style.visibility='hidden';
+  pauseEl.style.visibility='hidden';
 
+  var y = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = timerBreak - now;
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    timerEl.innerHTML = minutes + "m " + seconds + "s";
+    
+    if (distance < 0) {
+      clearInterval(y);
+      alarmSound.play();
+      timerEl.innerHTML = "Time's up!";
+      activateEl.style.visibility='visible';
+      pauseEl.style.visibility='visible';
+    }
+  }, 1000);
+})
 
