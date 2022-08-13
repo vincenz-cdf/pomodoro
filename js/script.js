332837d379296493d5e4079df819a43048e022ee
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2022-08-13 01:52:52 -0700

    more than 60 minutes

--- js/script.js
+++ js/script.js
@@ -46,10 +46,14 @@ activateEl.addEventListener("click", function(){
     var now = new Date().getTime();
 
     var distance = timerWork - now;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    timerEl.innerHTML = minutes + "m " + seconds + "s";
+    if(workValue < 60)
+      timerEl.innerHTML = minutes + "m " + seconds + "s";
+    else
+      timerEl.innerHTML = hours + "h " + minutes + "m " + seconds + "s";
 
     if (distance < 0) {
       clearInterval(x);
@@ -71,10 +75,14 @@ pauseEl.addEventListener("click", function() {
     var now = new Date().getTime();
 
     var distance = timerBreak - now;
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    timerEl.innerHTML = minutes + "m " + seconds + "s";
+    if(breakValue < 60)
+      timerEl.innerHTML = minutes + "m " + seconds + "s";
+    else
+      timerEl.innerHTML = hours + "h " + minutes + "m " + seconds + "s";
     
     if (distance < 0) {
       clearInterval(y);
